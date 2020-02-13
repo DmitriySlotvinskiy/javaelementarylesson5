40108dc691aa561b3a140b58620d7a639c1b9b7b
Dmitriy Slotvinskiy <slotvinskiy@gamil.com>
2020-02-13 09:36:10 +0200

    Exercise about string combinations altered

--- src/com/slotvinskiy/CombinationsInString.java
+++ src/com/slotvinskiy/CombinationsInString.java
@@ -1,4 +1,35 @@
 package com.slotvinskiy;
 
 public class CombinationsInString {
+
+    public static void main(String[] args) {
+
+        String string = "123";
+        printCombinations(string, 0, string.length());
+    }
+
+    private static void printCombinations(String string, int start, int end) {
+
+        if (end - start == 1) {
+            System.out.println(string);
+        } else {
+            for (int i = start; i < end; i++) {
+                if (i != start) {
+                    string = swap(string, start, i);
+                }
+                printCombinations(string, start + 1, end);
+                if (i != start) {
+                    string = swap(string, start, i);
+                }
+            }
+        }
+    }
+
+    private static String swap(String string, int i, int j) {
+        char[] temp = string.toCharArray();
+        char fixChar = temp[i];
+        temp[i] = temp[j];
+        temp[j] = fixChar;
+        return String.valueOf(temp);
+    }
 }
